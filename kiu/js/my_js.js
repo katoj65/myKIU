ae2374d631ecff9a6ddd75e44cd82ee0b5afaf86
katoj65 <katoj65@gmail.com>
2016-04-06 23:22:50 -0700

    Added files via upload

---
+++ kiu/js/my_js.js
@@ -0,0 +1,13 @@
+/**
+ * $(function(){
+ * $(window).scroll(function(){
+ * var scrolling=window.scrollY;
+ * if(scrolling>100){
+ * $('.header-top').css({'position':'fixed','height':'20'});    
+ * }     
+ * });    
+ *     
+ *     
+ *    
+ * });
+ */
