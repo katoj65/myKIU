1c9fd121988edf33cd1c48355d7101be75ed2f7e
katoj65 <katoj65@gmail.com>
2016-04-11 06:20:24 -0700

    Update config.php

--- kiu/modules/config.php
+++ kiu/modules/config.php
@@ -1,6 +1,10 @@
 <?php
 error_reporting(E_ALL ^ E_NOTICE);
 include_once('page.php');
+include_once('database-file.php');
+include_once('post.php');
+include_once('get.php');
+include_once('dynamic_css.php');
 
 
 
@@ -8,6 +12,62 @@ include_once('page.php');
  * @author lolkittens
  * @copyright 2016
  */
+
+function url_data($url){
+return strtolower(str_replace(' ','_',$url));
+}
+
+
+function url_string($url){
+return strtolower(str_replace('_',' ',$url));
+}
+
+
+function return_home(){
+return header('Location:index.php');    
+}
+
+
+
+function message($message){
+return($message);    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
